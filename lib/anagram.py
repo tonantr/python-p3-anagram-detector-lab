41ccdbb99c811cd6855acd65c8e3a6d1c06ddd70
tonan <tonantruong@hotmail.com>
2023-07-24 22:42:59 -0400

    completed anagram detector lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
-# your code goes here!
+
+class Anagram:
+
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, words):
+        matches = []
+        for i in words:
+            if sorted(i.lower()) == sorted(self.word.lower()):
+                matches.append(i)
+
+        return matches
+
